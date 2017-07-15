2fca234c3ce504d5e28d552d27c65e05821f8397
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-15 16:24:04 +0300

    modified

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const async = () => {
 const config = require('./config');
 
 async()
-    //  .then(() => require('./db').init(config.connectionString))
+    .then(() => require('./db').init(config.connectionString))
     .then((db) => require('./data').init(db))
     .then((data) => require('./app').init(data))
     .then((app) => {
-        app.listen(config.port, () => console.log(`server running`));
+        app.listen(config.port, () =>
+            console.log(`server running`));
     });
