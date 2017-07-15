9599893d20b30a06ff5812f99461d8253db95aa6
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-15 16:23:40 +0300

    modified

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const port = '3001';
-const connectionString = 'mongodb://localhost/items-db';
+const connectionString = 'mongodb://localhost/test';
 
-module.exports = {port, connectionString};
+module.exports = { port, connectionString };
