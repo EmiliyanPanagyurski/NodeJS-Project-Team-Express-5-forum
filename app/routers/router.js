3a7f778976f7c881c5369971777edfd157754228
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-16 00:10:25 +0300

    fixed app.post

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,8 +1,16 @@
 const init = (app, data) => {
-    require('./user.router').init(data);
+    require('./user.router').init(app, data);
 
-    app.get('./', (req, res) => {
+    app.get('/', (req, res) => {
         res.render('homepage');
+    });
+
+    app.get('/register', (req, res) => {
+        res.render('register');
+    });
+
+     app.get('/login', (req, res) => {
+        res.render('login');
     });
 };
 
