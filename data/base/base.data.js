4bc701427a1f231093c21f7c65dfa02ba03ee348
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-17 01:24:42 +0300

    modified

--- data/base/base.data.js
+++ data/base/base.data.js
@@ -1,17 +1,21 @@
 class BaseMongodbData {
-  constructor(db, ModelClass, validator) {
+  constructor(db, ModelClass) {
     this.db = db;
     this.ModelClass = ModelClass;
-    this.validator = validator;
     this.collectionName = this._getCollectionName();
     this.collection = this.db.collection(this.collectionName);
   }
 
   create(model) {
-    return this.db.collection.insert(model)
+    return this.collection.insert(model)
       .then(() => {
         return model;
       });
+  }
+
+  filterBy(props) {
+    return this.collection.find(props)
+      .toArray();
   }
 
   _getCollectionName() {
--- data/users.data.js
+++ data/users.data.js
@@ -5,6 +5,39 @@ class UsersData extends BaseData {
     constructor(db) {
         super(db, User);
     }
+
+    checkIfUsernameAndEmailAreFree(username, email) {
+        return this.filterBy()
+            .then((users) => {
+                for (let index = 0; index < users.length; index++) {
+                    if (users[index].username === username) {
+                        return {
+                            valid: false,
+                            msg: 'username is already taken',
+                        };
+                    }
+
+                    if (users[index].email === email) {
+                        return {
+                            valid: false,
+                            msg: 'email is already in use',
+                        };
+                    }
+                }
+                return {
+                    valid: true,
+                    msg: 'no errors',
+                };
+            });
+    }
+
+    comparePasswords(candidatePassword, realPassword, callback) {
+        if (candidatePassword !== realPassword) {
+            return callback(false);
+        }
+
+        return callback(true);
+    }
 }
 
 module.exports = UsersData;
