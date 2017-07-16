bf5bc2c0bc8fff90b87131c472518bacd7a466fc
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-17 01:26:30 +0300

    added user model implementation

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,9 @@
 class User {
-
+    constructor(username, password, email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
 
 module.exports = User;
