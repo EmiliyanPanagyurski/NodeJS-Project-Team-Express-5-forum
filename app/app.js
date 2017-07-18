79d4fc3eaa8c225c49e7bc94f46fc1b785bf3188
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-18 17:53:06 +0300

    modified

--- app/app.js
+++ app/app.js
@@ -1,30 +1,13 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
 
 const init = (data) => {
     const app = express();
 
     //  config
-    app.set('view engine', 'pug');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-
-    app.use(cookieParser());
-    app.use(session({
-        secret: 'secret',
-        saveUninitialized: true,
-        resave: true,
-    }));
-
-    app.use(passport.initialize());
-    app.use(passport.session());
+    require('./config').init(app, data);
+    require('./auth').init(app, data);
 
     app.use('/static', express.static('static'));
-    app.use(flash());
 
     // global var
     app.use( function(req, res, next) {
@@ -33,7 +16,6 @@ const init = (data) => {
         next();
     });
 
-    //  end config 
     require('./routers').init(app, data);
 
     return Promise.resolve(app);
