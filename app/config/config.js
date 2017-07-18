4bc7aff993498bd2f80c29f77d19379c1b816dd4
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-18 17:51:26 +0300

    app config moved to new module

---
+++ app/config/config.js
@@ -0,0 +1,14 @@
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+
+const init = (app, data) => {
+    app.set('view engine', 'pug');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(cookieParser('keyboard cat'));
+    app.use(flash());
+};
+
+module.exports = { init };
