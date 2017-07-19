74f15cbf7ceb0c9e5a5dc89616294d9c285655d3
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-19 15:05:05 +0300

    added method for public profile page

--- app/routers/user.router/controller.js
+++ app/routers/user.router/controller.js
@@ -1,44 +1,56 @@
-const UserModel = require('../../../models/user.model');
+const ObjectId = require('mongodb').ObjectId;
 
 const init = (data) => {
+    const UsersData = data.users;
     return {
         register: (req, res) => {
             const username = req.body.username;
             const password = req.body.password;
             const email = req.body.email;
-            const user = new UserModel(username, password, email);
-            const usersData = data.users;
-            usersData.checkIfUsernameAndEmailAreFree(user.username, user.email)
+            UsersData.checkIfUsernameAndEmailAreFree(username, email)
                 .then((validator) => {
                     if (!(validator.valid)) {
                         return res.render('register', { msg: validator.msg });
                     }
 
-                    return usersData.create({
-                        username: user.username,
-                        password: user.password,
-                        email: user.email,
+                    return UsersData.create({
+                        username: username,
+                        password: password,
+                        email: email,
                         firstName: '',
                         lastName: '',
                         signature: '',
                     }).then((createdUser) => {
-                        return res.render('profile', { id: createdUser._id });
+                        return res.render('login',
+                        { msgLogin: 'Successfull registration, now you can log in!' });
                     });
                 });
         },
-            updateProfile: (req, res) => {
-                const UsersData = data.users;
-                UsersData.update({ username: req.body.username },
-                    {
-                        firstName: req.body.firstname,
-                        lastName: req.body.lastname,
-                        signature: req.body.signature,
-                    })
-                    .then(() => {
-                        return res.redirect('/profile/'+ req.user._id);
-                    });
-            },
-
+        updateProfile: (req, res) => {
+            UsersData.update({ username: req.body.username },
+                {
+                    firstName: req.body.firstname,
+                    lastName: req.body.lastname,
+                    signature: req.body.signature,
+                })
+                .then(() => {
+                    return res.redirect('/profile/' + req.user._id);
+                });
+        },
+        getPublicProfile: (req, res) => {
+            UsersData.filterBy({ _id: new ObjectId(req.params.id) })
+                .then((user) => {
+                    return res.render('publicprofile',
+                        {
+                            username: user[0].username,
+                            firstName: user[0].firstName,
+                            lastName: user[0].lastName,
+                            email: user[0].email,
+                            signature: user[0].signature,
+                        }
+                    );
+                });
+        },
     };
 };
 
