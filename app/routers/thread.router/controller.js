8974c7f847843cb9ded0acdc6a9867ef6d6b5bf8
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-19 15:05:50 +0300

    modified

--- app/routers/thread.router/controller.js
+++ app/routers/thread.router/controller.js
@@ -7,18 +7,21 @@ const init = (data) => {
             const parent = req.params.id;
             const name = req.body.name;
             const createdBy = req.user[0].username;
+            const createdById = req.user[0]._id;
             const content = req.body.content;
             const createdOn = new Date();
             ThreadsData.create({
                 parent: parent,
                 name: name,
                 createdBy: createdBy,
+                createdById: createdById,
                 createdOn: createdOn,
                 content: content,
             }).then((createdThread) => {
                 PostsData.create({
                     parent: createdThread._id,
                     createdBy: createdThread.createdBy,
+                    createdById: createdThread.createdById,
                     content: createdThread.content,
                     createdOn: createdThread.createdOn,
                 }).then((createdpost) => {
