53d16d3fae95a3459f04fbbe0a6668ea55db3b97
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-22 18:29:52 +0300

    added creation ofmessage

--- data/data.js
+++ data/data.js
@@ -1,12 +1,14 @@
 const UsersData = require('./users.data');
 const ThreadsData = require('./threads.data');
 const PostsData = require('./posts.data');
+const MessageData = require('./message.data');
 
 const init = (db) => {
     return Promise.resolve({
         users: new UsersData(db),
         threads: new ThreadsData(db),
         posts: new PostsData(db),
+        messages: new MessageData(db),
     });
 };
 
