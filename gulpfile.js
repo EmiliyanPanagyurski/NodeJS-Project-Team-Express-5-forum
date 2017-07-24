bef367d854c731e1387bdd981b0abbea472e3984
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-07-24 20:37:52 +0300

    added gulp task for running tests

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const istanbul = require('gulp-istanbul');
+const mocha = require('gulp-mocha');
+
+gulp.task('pre-test', () => {
+    return gulp.src([
+        './data/**/*.js',
+        './app/**/*.js',
+        './config/**/*.js',
+        './db/**/*.js',
+        './models/**/*.js',
+        './server.js',
+    ])
+        .pipe(istanbul({
+            includeUntested: true,
+        }))
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('tests:unit', ['pre-test'], () => {
+    return gulp.src([
+        './test/unit/**/*.js',
+    ])
+        .pipe(mocha({
+            reporter: 'nyan',
+        }))
+        .pipe(istanbul.writeReports());
+});
